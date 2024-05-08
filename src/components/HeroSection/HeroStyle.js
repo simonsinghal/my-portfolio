a55b71fb09c8aec560d67b73cc62e2d910d1874a
Simon Singhal <simonsinghal@gmail.com>
2024-05-08 18:29:03 +0530

    Second Commit

--- src/components/HeroSection/HeroStyle.js
+++ src/components/HeroSection/HeroStyle.js
@@ -97,7 +97,7 @@ export const Img = styled.img`
   width: 100%;
   height: 100%;
   max-width: 400px;
-  max-height: 400px;
+  max-height: 900px;
   border-radius: 50%;
   border: 2px solid ${({ theme }) => theme.primary};
 
